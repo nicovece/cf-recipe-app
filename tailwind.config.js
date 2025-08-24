e8265afec55940979581af870468e82a522dc3a9
Nico Vece <me@nicovece.com>
2025-08-24 20:13:09 +0200

    Feat: Adds Tailwind CSS and base template
    Sets up Tailwind CSS for styling.
    Creates a base template for consistent structure across pages.
    Configures static file serving for development.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.py",
+    "./src/templates/**/*.html",
+    "./src/recipes/templates/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
