a239bcbdf844a97571bdca0a30e3ede0cfb5805a
Nico Vece <me@nicovece.com>
2025-08-24 21:03:07 +0200

    Feat: Adds browser-sync for live reloading and UI
    Adds browser-sync as a dev dependency to enable live reloading and a UI for easier development.
    
    Updates the "dev" script to start browser-sync, proxying the Django development server and watching for changes in HTML, Python, and CSS files.
    This automates browser refreshing upon code changes and enhances the development workflow.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,38 @@ module.exports = {
     "./src/recipes/templates/**/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ground_a: {
+          50: '#fdfbf7',
+          100: '#F7F0DE',
+          200: '#efe0c0',
+          300: '#e6d0a2',
+          400: '#D7B25B',
+          500: '#d1a94a',
+          600: '#c99a3a',
+          700: '#b8862e',
+          800: '#966e28',
+          900: '#7a5a26',
+        },
+        ground_b: '#c0a659',
+        alternate_b: '#a9c57c',
+        alternate_a: {
+          100: '#E2F3EE',
+          300: '#FBCCA6',
+          400: '#6fc3aa',
+        },
+        accent: {
+          400: '#F47F21',
+          600: '#E36F10',
+          800: '#61330D',
+          900: '#311907',
+        },
+      },
+      fontFamily: {
+        serif: ['Merriweather', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
